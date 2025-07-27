dd330e258cdbcfe815cdba113346a6e57ae95280
Amin Amin <nourmohammadi@neurotechcenter.org>
2025-07-27 19:23:56 +0000

    test

--- tests/generic/test_value_check.sql
+++ tests/generic/test_value_check.sql
@@ -1,5 +1,7 @@
-{% test value_check(model, column_name)%}
-SELECT * FROM 
-{{model}}
+{% test value_check(model, column_name) %} 
+
+SELECT * 
+FROM {{model}}
 WHERE {{column_name}} < 1e4
+
 {% endtest %}
