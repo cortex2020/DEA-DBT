fd9d0bb942b1d9d248a810d5acd3d84d91ac87f2
Amin Amin <nourmohammadi@neurotechcenter.org>
2025-12-29 14:26:02 +0000

    Transform table code change

--- macros/copy_into_snowflake.sql
+++ macros/copy_into_snowflake.sql
@@ -1,9 +1,10 @@
-{% macro copy_json(table_nm) %} 
+{% macro copy_json(table_nm) %}
 
---Delete the data from the copy table before running the copy command
-delete from {{var ('target_db') }}.{{var ('target_schema')}}.{{ table_nm }}; 
+    -- Delete the data from the copy table before running the copy command
+    delete from {{ var("target_db") }}.{{ var("target_schema") }}.{{ table_nm }}
+    ;
 
---Copy the data from the snowflake external stage to snowflake table
+    -- Copy the data from the snowflake external stage to snowflake table
 COPY INTO {{var ('target_db') }}.{{var ('target_schema')}}.{{ table_nm }}
 
 FROM
