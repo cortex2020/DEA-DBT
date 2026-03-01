8fb4c440814ae392762fdce91b192dfdde52e461
Amin Amin <nourmohammadi@neurotechcenter.org>
2026-03-01 21:37:59 +0000

    finished walmart_date_dim model

--- macros/copy_department_raw.sql
+++ macros/copy_department_raw.sql
@@ -1,41 +1,31 @@
 {% macro copy_department_raw() %}
-
-  {% set copy_sql %}
-
-    copy into {{ source('bronze', 'DEPARTMENT_RAW') }}
-    (
-      store_id,
-      dept_id,
-      dept_date,
-      weekly_sales,
-      isholiday,
-      insert_dts,
-      update_dts,
-      source_file_name,
-      source_file_row_number
-    )
-    from (
-      select
-        $1::int                  as store_id,
-        $2::int                  as dept_id,
-        $3::date                 as dept_date,
-        $4::number               as weekly_sales,
-        $5::boolean              as isholiday,
-        current_timestamp()      as insert_dts,
-        current_timestamp()      as update_dts,
-        metadata$filename        as source_file_name,
-        metadata$file_row_number as source_file_row_number
-      from @DEA_WAL_DB.BRONZE.S3_WALMART_STAGE/department/
-    )
-    file_format = (format_name = DEA_WAL_DB.BRONZE.CSV_FORMAT)
-    on_error = 'abort_statement'
-    force = false
-    ;
-
-  {% endset %}
-
-  {% do log(copy_sql, info=True) %}
-  {% set res = run_query(copy_sql) %}
-  {% do return(res) %}
-
+  copy into {{ source('bronze', 'DEPARTMENT_RAW') }}
+  (
+    store_id,
+    dept_id,
+    dept_date,
+    weekly_sales,
+    isholiday,
+    insert_dts,
+    update_dts,
+    source_file_name,
+    source_file_row_number
+  )
+  from (
+    select
+      $1::int                  as store_id,
+      $2::int                  as dept_id,
+      $3::date                 as dept_date,
+      $4::number               as weekly_sales,
+      $5::boolean              as isholiday,
+      current_timestamp()      as insert_dts,
+      current_timestamp()      as update_dts,
+      metadata$filename        as source_file_name,
+      metadata$file_row_number as source_file_row_number
+    from @DEA_WAL_DB.BRONZE.S3_WALMART_STAGE/department/
+  )
+  file_format = (format_name = DEA_WAL_DB.BRONZE.CSV_FORMAT)
+  on_error = 'abort_statement'
+  force = false
+  ;
 {% endmacro %}
---
+++ models/silver/walmart_date_dim.sql
@@ -0,0 +1,35 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'store_date',
+    incremental_strategy = 'merge',
+    pre_hook = "{{ copy_department_raw() }}"
+) }}
+
+
+with src as (
+    select
+        dept_date as store_date,
+        isholiday
+    from {{ source('bronze', 'DEPARTMENT_RAW') }}
+
+    {% if is_incremental() %}
+      where dept_date > (select coalesce(max(store_date), '1900-01-01'::date) from {{ this }})
+    {% endif %}
+
+),
+
+dedup as (
+    select
+        store_date,
+        max(isholiday::int)::boolean as isholiday
+    from src
+    group by 1
+)
+
+select
+    {{ dbt_utils.generate_surrogate_key(['store_date']) }} as date_id,
+    store_date,
+    isholiday,
+    current_timestamp() as insert_date,
+    current_timestamp() as update_date
+from dedup
