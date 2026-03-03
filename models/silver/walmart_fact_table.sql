ef1f1c6a7549db701bbe8635f5c665252957319c
Amin Amin <nourmohammadi@neurotechcenter.org>
2026-03-03 20:13:04 +0000

    updated columns in fact table to include store_date

--- models/silver/walmart_fact_table.sql
+++ models/silver/walmart_fact_table.sql
@@ -1,6 +1,10 @@
+{{ config(materialized='view') }}
+
 select
   store_id,
   dept_id,
+  date_id,
+  store_size,
   store_weekly_sales,
   fuel_price,
   store_temperature,
@@ -11,10 +15,8 @@ select
   markdown3,
   markdown4,
   markdown5,
-  dbt_valid_from  as insert_date,
-  dbt_updated_at  as update_date,
   dbt_valid_from  as vrsn_start_date,
-  dbt_valid_to    as vrsn_end_date
+  dbt_valid_to    as vrsn_end_date,
+  dbt_valid_from  as insert_date,
+  dbt_updated_at  as update_date
 from {{ ref('walmart_fact_snapshot') }}
-
-
--- snapshots/walmart_fact_snapshot.sql
+++ snapshots/walmart_fact_snapshot.sql
@@ -3,8 +3,9 @@
 {{
   config(
     strategy='check',
-    unique_key="store_id || '-' || dept_id || '-' || to_varchar(fact_date)",
+    unique_key="store_id || '-' || dept_id || '-' || date_id",
     check_cols=[
+      'store_size',
       'store_weekly_sales',
       'fuel_price',
       'store_temperature',
@@ -12,14 +13,18 @@
       'cpi',
       'markdown1','markdown2','markdown3','markdown4','markdown5'
     ],
-    pre_hook=["{{ copy_fact_raw() }}", "{{ copy_department_raw() }}"]
+    pre_hook=[
+      "{{ copy_fact_raw() }}",
+      "{{ copy_department_raw() }}"
+    ]
   )
 }}
 
 select
   f.store_id,
   d.dept_id,
-  f.fact_date,
+  dd.date_id,
+  sd.store_size,
   d.weekly_sales as store_weekly_sales,
   f.fuel_price,
   f.temperature as store_temperature,
@@ -34,5 +39,10 @@ from {{ source('bronze','fact_raw') }} f
 join {{ source('bronze','department_raw') }} d
   on d.store_id = f.store_id
  and d.dept_date = f.fact_date
+join {{ ref('walmart_date_dim') }} dd
+  on dd.store_date = f.fact_date
+join {{ ref('walmart_store_dim') }} sd
+  on sd.store_id = f.store_id
+ and sd.dept_id  = d.dept_id
 
 {% endsnapshot %}
